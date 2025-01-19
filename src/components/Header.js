d043b4446d9d6b2115ec923c0e06fff0e673d73d
113932_Cremona_Juan <juan_crub@outlook.com>
2025-01-18 21:39:54 -0300

    movile.- retract menu when choice an option

--- src/components/Header.js
+++ src/components/Header.js
@@ -28,13 +28,13 @@ const Header = () => {
 
       <div className={`collapse navbar-collapse ${isOpen ? 'show' : ''}`} id="navbarNav">
         <ul className="navbar-nav ml-auto">
-          <li className="nav-item">
+          <li className="nav-item" onClick={toggleMenu}>
             <Link className="nav-link" to="/dashboard">Dashboard</Link>
           </li>
-          <li className="nav-item">
+          <li className="nav-item" onClick={toggleMenu}> 
             <Link className="nav-link" to="/performance">Performance</Link>
           </li>
-          <li className="nav-item">
+          <li className="nav-item" onClick={toggleMenu}>
             <Link className="nav-link" to="/login">Login</Link>
           </li>
           
