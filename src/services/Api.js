979079fecdd3564c2a7261f74a0cc2048c055d49
113932_Cremona_Juan <juan_crub@outlook.com>
2025-05-09 15:21:03 -0300

    errors

--- src/services/Api.js
+++ src/services/Api.js
@@ -10,6 +10,9 @@ const api = axios.create({
 export const getEMAs = async () => {
   try {
     const response = await api.get('api/v1/historical-emas'); 
+    console.log('Respuesta de la API:', response); // Verifica si la respuesta está correctamente formada
+    console.log('response.data:', response.data); // Verifica que 'data' está presente
+    console.log('response.data.data:', response.data.data); // Verifica si 'data' dentro de 'data' est
     return response.data.data;
   } catch (error) {
     console.error('Error fetching EMAs:', error);
